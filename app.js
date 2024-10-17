5b7faad87c5bd8dfc285103576d4fe0c8394e7f2
Fernanda Ishida <fer.ishida@gmail.com>
2024-10-17 11:27:11 -0300

    #2 Data, templates, click events, conditional rendering

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('hello, vue')
+const app = Vue.createApp({
+    data() {
+        return {
+            showBooks: true,
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45
+        }
+    },
+    methods: {
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        }
+        
+    }
+})
+
+app.mount('#app')
