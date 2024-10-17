2a5d9def9732f188513390ac208cda90ac92c780
Fernanda Ishida <fer.ishida@gmail.com>
2024-10-17 13:26:09 -0300

    #3 v-for, v-bind, dynamic classes

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
 const app = Vue.createApp({
     data() {
         return {
+            https: '//www.thenetninja.co.uk',
             showBooks: true,
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            x: 0,
-            y: 0
-
+            books: [
+                { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true},
+                { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
+                { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true }
+            ]
         }
     },
     methods: {
         toggleShowBooks() {
-            this.showBooks = !this.showBooks
-        },
-        handleEvents(e, data) {
-            console.log(e, e.type)
-            if(data){
-                console.log(data)
-            }
-        },
-        handleMousemove(e) {
-            this.x = e.offsetX
-            this.y =e.offsetY
-        }
-        
+            this.showBooks = !this.showBooks  
     }
+}
 })
 
 app.mount('#app')
